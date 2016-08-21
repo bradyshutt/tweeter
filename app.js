ce3a3b146d1429f061d600c993cf6fdf0e3f99f0
Brady <bradyshutt@gmail.com>
2016-08-21 15:54:55 -0700

    Implemented promises instead of callbacks for initDB and examineRequest. Abandoning the StandardJS style guide.

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-/* TODO: all err arg to all callbacks to adhere to standards */
-/* TODO: change all '//' comments to '/*' ones */
-
 'use strict'
 
 /* global cpr */
@@ -20,40 +17,46 @@ http.createServer((req, res) => {
   /* If requesting a static file, skip cookie parsing,
    * db initialization, and session validation */
   if ((/^\/static\//).test(req.url) || (/^\/images\//).test(req.url)) {
-    // cpr('Recieved request for asset: y[' + req.url + ' ]')
     cpr.inc(req.url)
     res.redirect = utils.redirectURL
     routeFile(req, res)
-  } else {
+  }
+  else {
     cpr('')
     cpr.inc(req.url)
-    // cpr('Recieved request for URL: y[' + req.url + ' ]')
-    examineRequest(req, res, () => {
-      models.initDB(() => {
-        cpr('User is ' + (req.validated ? 'g[' : 'r[not ') + 'logged in.]')
-        route(req, res)
-      })
+
+    Promise.all([
+      examineRequest(req, res),
+      models.initDB()
+    ])
+
+    .then(([userValidated, dbInitP]) => {
+      cpr('User is ' + (userValidated ? 'g[' : 'r[not ') + 'logged in.]')
+      route(req, res)
     })
   }
 }).listen(8000)
 
-function examineRequest (req, res, cb) {
+function examineRequest (req, res) {
   cookies.parse(req)
   res.setCookie = cookies.responseSetCookie
   res.removeCookie = cookies.responseRemoveCookie
   res.redirect = utils.redirectURL
 
-  if ((req.username = req.cookies['username'] || null)) {
-    if (req.cookies['sessionkey']) {
-      users.validateSession(req, (err, isValid) => {
-        if (err) throw err
-        req.validated = isValid
-        cb()
-      })
+  return new Promise((resolve, reject) => {
+    if ((req.username = req.cookies['username'] || null)) {
+      if (req.cookies['sessionkey']) {
+        users.validateSession(req, (err, isValid) => {
+          if (err) throw err
+          req.validated = isValid
+          resolve(true)
+        })
+      }
+    } else {
+      resolve(false)
     }
-  } else {
-    cb()
-  }
-  // if (req.method === 'POST') { /* TODO: SCAN FORM DATA */ }
+    // if (req.method === 'POST') { /* TODO: SCAN FORM DATA */ }
+  })
+
 }
 
--- models.js
+++ models.js
@@ -5,40 +5,41 @@ var db = new sql.Database('database.db')
 
 function initDB (cb) {
   cpr.db('Initializing Database')
-  db.serialize(() => {
-    db.run('PRAGMA foreign_keys = ON')
 
-    db.run(
-      'CREATE TABLE IF NOT EXISTS users (' +
-      'username TEXT PRIMARY KEY NOT NULL, ' +
-      'firstName TEXT NOT NULL, ' +
-      'lastName TEXT NOT NULL, ' +
-      'email TEXT, ' +
-      'gender TEXT, ' +
-      'profilePicture TEXT, ' +
-      'password TEXT NOT NULL)'
-    )
+  return new Promise((resolve, reject) => {
+    db.serialize(() => {
+      db.run('PRAGMA foreign_keys = ON')
 
-    db.run(
-      'CREATE TABLE IF NOT EXISTS sessions (' +
-      'username TEXT, ' +
-      'sessionKey TEXT NOT NULL, ' +
-      'FOREIGN KEY(username) REFERENCES users(username))'
-    )
+      db.run(
+        'CREATE TABLE IF NOT EXISTS users (' +
+        'username TEXT PRIMARY KEY NOT NULL, ' +
+        'firstName TEXT NOT NULL, ' +
+        'lastName TEXT NOT NULL, ' +
+        'email TEXT, ' +
+        'gender TEXT, ' +
+        'profilePicture TEXT, ' +
+        'password TEXT NOT NULL)'
+      )
 
-    db.run(
-      'CREATE TABLE IF NOT EXISTS posts (' +
-      'postID INTEGER PRIMARY KEY, ' +
-      'username TEXT, ' +
-      'numLikes INTEGER, ' +
-      'postDate TEXT NOT NULL, ' +
-      'postContent TEXT NOT NULL, ' +
-      'FOREIGN KEY(username) REFERENCES users(username))',
-      /*
-       * Run callback after last serialized query is finished
-       */
-      cb()
-    )
+      db.run(
+        'CREATE TABLE IF NOT EXISTS sessions (' +
+        'username TEXT, ' +
+        'sessionKey TEXT NOT NULL, ' +
+        'FOREIGN KEY(username) REFERENCES users(username))'
+      )
+
+      db.run(
+        'CREATE TABLE IF NOT EXISTS posts (' +
+        'postID INTEGER PRIMARY KEY, ' +
+        'username TEXT, ' +
+        'numLikes INTEGER, ' +
+        'postDate TEXT NOT NULL, ' +
+        'postContent TEXT NOT NULL, ' +
+        'FOREIGN KEY(username) REFERENCES users(username))',
+        /* Resolve promise after last serialized query is finished */
+        () => resolve('DB Finished Initialization')
+      )
+    })
   })
 }
 
